9fdaea68aef73f653187ad8f5169addca5609ecd
Travis Brown <tychoterra@protonmail.com>
2023-01-12 23:49:14 -0500

    chnaged my mind about the title. Also thinking of using typescript for this.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
-
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="title">FactSearch</h1>
+      <h1 className="title">FactFinder</h1>
       <input type="text" placeholder=" Search" className="search" />
     </header>
-  )
+  );
 }
